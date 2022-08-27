4810dd84ef10ffc8a6d4ee9014c4885d2dcbbafc
Atilla ŞİMŞEK <info@atillakurtulussimsek.com.tr>
2022-08-27 09:54:22 +0300

    add config github url

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -35,9 +35,7 @@ const config = {
         docs: {
           sidebarPath: require.resolve('./sidebars.js'),
           routeBasePath: '/',
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          //editUrl: 'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+          editUrl: 'https://github.com/varkhedigital/LibraSoft_Docs/tree/main/',
         },
         blog: false,
         theme: {
