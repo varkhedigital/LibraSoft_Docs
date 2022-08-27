94013125a5335ccbdd33d0048e892e0079804cf1
Atilla ŞİMŞEK <info@atillakurtulussimsek.com.tr>
2022-08-27 17:54:04 +0300

    start command change

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
     apps : [
         {
             name: "LibraSoftDocs",
-            script: "npm start",
+            script: "npm run serve -- --port 3320 --host 0.0.0.0",
             watch: false,
             env: {}
         }
